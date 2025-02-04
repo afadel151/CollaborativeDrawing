24aa17f6ff263db40d59127c34551def0225821e
afadel151 <af8356207@gmail.com>
2025-02-04 13:54:28 +0100

    add .nuxt

---
+++ .nuxt/types/supabase.d.ts
@@ -0,0 +1,6 @@
+declare module '#supabase/server' {
+  const serverSupabaseClient: typeof import('/home/fadel/Github/collaborative_drawing/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
+  const serverSupabaseServiceRole: typeof import('/home/fadel/Github/collaborative_drawing/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
+  const serverSupabaseUser: typeof import('/home/fadel/Github/collaborative_drawing/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
+  const serverSupabaseSession: typeof import('/home/fadel/Github/collaborative_drawing/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
+}
